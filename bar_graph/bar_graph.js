e43247eb2536d12347d9c0a9c988b371ff2d6454
Ruturaj Gole <ruturajpokemon1@gmail.com>
2020-03-16 16:46:31 -0600

    Cleanup unused files

--- bar_graph/bar_graph.js
+++
@@ -1,8 +0,0 @@
-//importing json utility from d3
-import {select, json} from 'd3';
-
-const svg = select('svg');
-const width = +svg.attr('width');
-const height = +svg.attr('height');
-
-// loading 
